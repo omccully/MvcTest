2c4362f90a2d6cbff525496983b7f390f9616a0a
omccully <oscarmccully@gmail.com>
2023-06-30 13:25:36 -0500

    add version display

---
+++ MvcTest/Library/AppVersionHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace MvcTest.Library
+{
+    public static class AppVersionHelper
+    {
+        public static string GetVersion()
+        {
+            return Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                ?? "Unknown";
+        }
+    }
+}
