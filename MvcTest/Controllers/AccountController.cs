196b8ea93546c7a1c5a3a46aa5b2147c908131b8
omccully <oscarmccully@gmail.com>
2023-06-30 00:59:04 -0500

    add account functionality

---
+++ MvcTest/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcTest.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
